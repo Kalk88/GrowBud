0982e5e661497238a63fc71fc1ba7000e63388f7
petter <petter.mansson1@gmail.com>
2019-10-09 21:02:53 +0200

    Add accountRegistration route

---
+++ client/src/components/UserSignUpEdit.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="userSignUp-Edit-wrapper">
+    <h1> Create a free account </h1>
+    <input 
+      type="text" 
+      class="username" 
+      placeholder=" username"
+      v-model="User.username"/>
+    <input 
+      type="email" 
+      class="email" 
+      placeholder="email" 
+      v-model="User.email"/>
+    <input 
+      type="password" 
+      class="password" 
+      placeholder="password"
+      v-model="User.password"/>
+
+    <button 
+    class="save" 
+    type="button" 
+    value="SignUp"
+    style="width: 10%; align-self: center; background-color: black; color:hotpink; margin-top: 1rem;"
+    @click="register()"
+    >SignUp
+    </button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapMutations, mapGetters, mapActions } from 'vuex';
+import API from '../api/api';
+
+export default {
+  name: 'UserSignUpEdit',
+  components: {
+    
+  },
+
+  data(){
+    return{
+      User:{ 
+        username: "",
+        email: "",
+        password: "",
+      }
+    }
+
+  },
+  methods: {
+
+    register() {
+      API.accountRegister(this.User).then(data =>{
+        console.log(data);
+      }
+        
+      );
+    },
+
+    ...mapMutations(["setUser"]),
+  },
+ 
+}
+</script>
+
+<style scoped>
+
+.userSignUp-Edit-wrapper{
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+
+input{
+  width: 30%;
+  align-self: center;
+  margin-top: 1rem;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Wollolooo
-    "/>
+    <UserSignUpEdit/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import UserSignUpEdit from '@/components/UserSignUpEdit.vue'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    UserSignUpEdit
   }
 }
 </script>
