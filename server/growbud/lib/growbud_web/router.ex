27342d0bf13efe731c0b84e597c3c75524921381
Kalk <simon.d.mansson@gmail.com>
2019-10-09 19:28:56 +0200

    basic graphql structure

--- server/growbud/lib/growbud_web/router.ex
+++ server/growbud/lib/growbud_web/router.ex
@@ -19,8 +19,9 @@ defmodule GrowbudWeb.Router do
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", GrowbudWeb do
-  #   pipe_through :api
-  # end
+  forward "/graphiql", Absinthe.Plug.GraphiQL,
+    schema: GrowbudWeb.Schema
+
+  forward "/api", Absinthe.Plug,
+    schema: GrowbudWeb.Schema
 end
--- server/growbud/mix.exs
+++ server/growbud/mix.exs
@@ -44,7 +44,9 @@ defmodule Growbud.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:bcrypt_elixir, "~> 2.0"},
-      {:absinthe, "~> 1.4.0"}
+      {:absinthe, "~> 1.4.0"},
+      {:absinthe_plug, "~> 1.4.0"},
+      {:poison, "~> 2.1.0"},
     ]
   end
 
