5b4e84e29aae7905a0e7e6364956d5a4450bd942
Kalk <simon.d.mansson@gmail.com>
2019-10-12 20:40:16 +0200

    Register user
    A user can be registered through the api endpoint with a mutation
    e.g.
    mutationn {registerUser(name: "joe", password: "ddd", email: "t@ex") {
      name
    }}

--- server/growbud/test/growbud/accounts_test.exs
+++ server/growbud/test/growbud/accounts_test.exs
@@ -6,7 +6,6 @@ defmodule Growbud.AccountsTest do
   describe "users" do
     alias Growbud.Accounts.User
 
-    @valid_attrs %{name: "some name"}
     @valid_create %{name: "some name", email: "test@growbud.email", password: "somefancypassword"}
     @update_attrs %{name: "some updated name"}
     @invalid_attrs %{name: nil}
@@ -14,7 +13,7 @@ defmodule Growbud.AccountsTest do
     def user_fixture(attrs \\ %{}) do
       {:ok, user} =
         attrs
-        |> Enum.into(@valid_attrs)
+        |> Enum.into(@valid_create)
         |> Accounts.register_user()
 
       user
@@ -28,16 +27,6 @@ defmodule Growbud.AccountsTest do
     test "get_user!/1 returns the user with given id" do
       user = user_fixture()
       assert Accounts.get_user!(user.id) == user
-    end
-
-    test "register_user/1 with valid data creates a user" do
-      assert {:ok, %User{} = user} = Accounts.register_user(@valid_create)
-      assert user.name == "some name"
-      assert user.roles.name == "User"
-    end
-
-    test "register_user/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.register_user(@invalid_attrs)
     end
 
     test "update_user/2 with valid data updates the user" do
@@ -62,11 +51,29 @@ defmodule Growbud.AccountsTest do
       user = user_fixture()
       assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
+  end
+
+  describe "registration" do
+    alias Growbud.Accounts.Registration
+
+    @valid_attrs %{email: "some@email", name: "some name", password: "some password"}
+    @invalid_attrs %{email: nil, name: nil, password: nil}
+
+    test "register_user/1 with valid data creates a user" do
+      assert {:ok, %Registration{} = registration} = Accounts.register_user(@valid_attrs)
+      assert registration.email == "some@email"
+      assert registration.name == "some name"
+    end
+
+    test "register_user/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.register_user(@invalid_attrs)
+    end
 
     test "register_admin/1 with valid data creates a user" do
-      assert {:ok, %User{} = user} = Accounts.register_admin(@valid_create)
-      assert user.name == "some name"
-      assert user.roles.name == "Administrator"
+      assert {:ok, %Registration{} = registration} = Accounts.register_admin(@valid_attrs)
+      assert registration.name == "some name"
+      assert registration.email == "some@email"
+      assert registration.roles.name == "Administrator"
     end
   end
 end
