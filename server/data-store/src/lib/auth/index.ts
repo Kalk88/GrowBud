1de22f2f5dbe7f789baf521056693f01da47ecc5
Simon Månsson <simon.d.mansson@gmail.com>
2019-11-04 21:51:17 +0100

    Feature/cheapskate backend (#41)
    * It's free real estate
    
    * Auth module
    
    * Change suffix on index
    
    * Deploy action for datastore
    
    * Fix spelling
    
    * Add Typescript config
    
    * update ignorefile
    
    * Update tsc output and npm run
    
    * Add login, logut(partial) and register
    
    * Return JWT on user account creation
    
    * Add dockerfile for local development
    
    * Remove unused function
    
    * Actually add a dockerfile
    
    * Add generic error handler

---
+++ server/data-store/src/lib/auth/index.ts
@@ -0,0 +1,57 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+export interface Credential {
+  email: string,
+  password: string
+}
+
+interface firebaseError {
+  code: string,
+  message: string
+}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOVXtaV1OsZp3YWwH_d3fiL2pdbGUuQH8",
+  authDomain: "growbud-50ed4.firebaseapp.com",
+  databaseURL: "https://growbud-50ed4.firebaseio.com",
+  projectId: "growbud-50ed4",
+  storageBucket: "growbud-50ed4.appspot.com",
+  messagingSenderId: "1032847847071",
+  appId: "1:1032847847071:web:116560c04ed237deb9bcaa",
+  measurementId: "G-N5LWZZRVS4"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+export async function login(credential: Credential): Promise<string> {
+  try {
+    const result = await firebase.auth().signInWithEmailAndPassword(credential.email, credential.password)
+    if (result.user) {
+      const token = await result.user.getIdToken(true)
+      console.log(token)
+      return token
+    }
+  } catch (error) {
+    console.error('Unable to authorize user: ', formatError(error))
+    throw (new Error('Authentication error'))
+  }
+  return new Promise((resolve, _) => resolve(''))
+}
+
+export async function registerCredentials(credential: Credential): Promise<string> {
+  try {
+    const result = await firebase.auth().createUserWithEmailAndPassword(credential.email, credential.password)
+    if (result.user) {
+      return result.user.getIdToken(true)
+    }
+  } catch (error) {
+    console.error('Unable to register credentials: ', formatError(error))
+    throw (new Error('Authentication error'))
+  }
+  return new Promise((resolve, _) => resolve(''))
+}
+
+function formatError(error: firebaseError): string {
+  return `code: ${error.code} msg: ${error.message}`
+}
---
+++ server/data-store/src/schema/index.ts
@@ -0,0 +1,84 @@
+import {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLBoolean
+} from 'graphql'
+
+import {
+    login,
+    Credential,
+    registerCredentials
+} from '../lib/auth'
+
+const gqlString = { type: GraphQLString }
+
+const JWTType = new GraphQLObjectType({
+    name: 'JWT',
+    description: 'JWT',
+    fields: () => ({
+        JWT: gqlString
+    })
+})
+
+const RegistrationType = new GraphQLObjectType({
+    name: 'Registration',
+    description: 'Registration details',
+    fields: () => ({
+        JWT: gqlString,
+        email: gqlString,
+        userName: gqlString
+    })
+})
+
+
+const queryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Query operations',
+    fields: () => ({
+        test: { type: GraphQLBoolean }
+    })
+})
+
+const mutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Mutation operations',
+    fields: () => ({
+        login: {
+            description: 'Login with the given credentials, returns a JWT',
+            type: JWTType,
+            args: {
+                email: gqlString,
+                password: gqlString
+            },
+            resolve: async (_root, args) => {
+                const JWT = await login(args as Credential)
+                return { JWT }
+            }
+        },
+        register: {
+            description: 'Register an account.',
+            type: RegistrationType,
+            args: {
+                email: gqlString,
+                password: gqlString,
+                userName: gqlString,
+            },
+            resolve: async (_root, args) => {
+                const cred: Credential = { email: args.email, password: args.password }
+                const JWT = await registerCredentials(cred)
+                return {
+                    JWT,
+                    email: args.email,
+                    userName: args.userName
+                }
+            }
+
+        }
+    })
+})
+
+export default new GraphQLSchema({
+    mutation: mutationType,
+    query: queryType
+})
