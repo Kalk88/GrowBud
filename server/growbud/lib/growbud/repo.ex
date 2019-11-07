d8989da85f261ca7b9ed206aef9637718be01cda
Simon Månsson <simon.d.mansson@gmail.com>
2019-11-07 18:11:51 +0100

    Remove elixir backend (#42)
    Since we are not using elixir for the backend we should remove it.

--- server/growbud/lib/growbud/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Growbud.Repo do
-  use Ecto.Repo,
-    otp_app: :growbud,
-    adapter: Ecto.Adapters.Postgres
-end
--- server/growbud/test/growbud_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GrowbudWeb.PageControllerTest do
-  use GrowbudWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
