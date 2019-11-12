d181903ae239975aeef54a7686b788d2ccc5190f
Kalk <simon.d.mansson@gmail.com>
2019-11-12 18:55:59 +0100

    Add function finding schedules and passing messages

--- server/cloud-functions/notifySchedulesInRange.js
+++ server/cloud-functions/notifySchedulesInRange.js
@@ -4,10 +4,15 @@ const firestore = new Firestore({
     projectId: 'growbud-50ed4'
 })
 const pubsub = new PubSub();
+<<<<<<< HEAD
+=======
+const topicName = 'wateringSchedules'
+>>>>>>> Add function finding schedules and passing messages
 
 /**
  * Finds all schedules in range of a set time period and passes messages to be sent to pubsub
  */
+<<<<<<< HEAD
 exports.notifySchedulesInRange = (req, res) => {
     firestore.collection('wateringSchedules')
         .where('timestamp', '>=', req.body.start)
@@ -25,6 +30,25 @@ exports.notifySchedulesInRange = (req, res) => {
             console.log('Error sending message:', error)
             res.sendStatus(500)
         })
+=======
+exports.notifySchedulesInRange = async (req, res) => {
+    try {
+        const schedulesRef = firestore.collection('wateringSchedules')
+        const snapshot = await schedulesRef
+            .where('timestamp', '>=', req.body.start)
+            .where('timestamp', '<=', req.body.end)
+            .get()
+
+        const messages = snapshot.docs.map(doc => formatMessage(doc))
+        const messagesBuffer = Buffer.from(messages);
+        const messageId = await pubsub.topic(topicName).publish(messagesBuffer)
+        console.log(`Message ${messageId} published.`)
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        console.log('Error sending message:', error)
+    }
+>>>>>>> Add function finding schedules and passing messages
 }
 
 function formatMessage(doc) {
