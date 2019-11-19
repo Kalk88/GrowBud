f2c6132ec4fd0ee2cbcf5e648e73234a620ae01a
Kalk <simon.d.mansson@gmail.com>
2019-11-19 21:29:23 +0100

    Add refresh token route

--- server/data-store/src/lib/auth/index.ts
+++ server/data-store/src/lib/auth/index.ts
@@ -1,4 +1,6 @@
-import firebase from '../firebaseClient';
+import firebase from '../firebaseClient'
+import axios from 'axios'
+import qs from 'qs'
 
 export interface Credential {
   email: string,
@@ -9,6 +11,12 @@ export interface userInfo {
   JWT: string,
   JWTExpiry: string,
   id: string,
+  refreshToken: string
+}
+
+export interface RefreshInfo {
+  JWT: string,
+  JWTExpiry: number,
   refreshToken: string
 }
 
@@ -44,8 +52,34 @@ export async function registerCredentials(credential: Credential): Promise<userI
   }
 }
 
+export async function refreshToken(token: string): Promise<RefreshInfo> {
+  try {
+    const payload = {
+      grant_type: 'refresh_token',
+      refresh_token: token
+    }
+    const response: any = await axios({
+      method: "POST",
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: qs.stringify(payload),
+      url: `https://securetoken.googleapis.com/v1/token?key=${process.env.FIREBASEAPIKEY}`,
+    })
+
+    const data = response.data
+    return {
+      JWT: data.id_token,
+      JWTExpiry: Date.now() + data.expires_in,
+      refreshToken: data.refresh_token
+    }
+  } catch (error) {
+    console.error(error)
+    throw (new Error('Authentication error'))
+
+  }
+}
+
 async function userDTO(user: firebase.User): Promise<userInfo> {
-  const { token, expirationTime } = await user.getIdTokenResult(true)
+  const { token, expirationTime } = await user.getIdTokenResult()
   return {
     JWT: token,
     JWTExpiry: expirationTime,
