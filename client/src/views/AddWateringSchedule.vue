cff9a63395c427dfb7a61752da94d665287348c6
petter <petter.mansson1@gmail.com>
2019-11-26 20:17:10 +0100

    Add components for wateringschedule

---
+++ client/src/views/AddWateringSchedule.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <q-input label="Name the plant are you going to water" 
+    outlined
+    standout />
+    <q-date 
+      v-model="date"
+      :dark="true"
+      today-btn
+      color="green"
+    />
+    <q-btn-group>
+      <q-btn label="Days" />
+      <q-btn label="Weeks"/>
+    </q-btn-group>
+    <IncrementerButton />
+  </div>
+</template>
+
+<script>
+
+import {
+  QDate,
+  QInput,
+  QBtnGroup,
+  QBtn
+} from 'quasar'
+import IncrementerButton from '../components/IncrementerButton.vue'
+
+export default {
+  name: "AddWateringSchedule",
+  components: {
+    QDate,
+    QInput,
+    QBtnGroup,
+    QBtn,
+    IncrementerButton
+  },
+  data() {
+    return {
+      date: "",
+    };
+  },
+  mounted() {
+    this.date = new Date();
+  }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
