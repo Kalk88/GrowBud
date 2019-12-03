88e63493d2687c5d554ac5fdc611fa7924430197
Simon Månsson <simon.d.mansson@gmail.com>
2019-12-03 17:07:35 +0100

    Add function for removing watering schedule (#69)
    * Add function for removing watering schedule
    
    * Return boolean on delete mutation

--- server/data-store/src/lib/db/index.ts
+++ server/data-store/src/lib/db/index.ts
@@ -57,6 +57,18 @@ export async function scheduleWateringFor(plant: object, userId: string, timesta
     }
 }
 
+export async function removeWateringScheduleById(id: string): Promise<object> {
+    try {
+        await db.collection('wateringSchedules')
+            .doc(id)
+            .delete()
+    } catch (error) {
+        console.error(error)
+        return { status: false }
+    }
+    return { status: true }
+}
+
 function snapshotToSchedule(doc: firebase.firestore.QueryDocumentSnapshot | firebase.firestore.DocumentSnapshot): WateringSchedule {
     return Maybe.Just(doc.data()).matchWith({
         Just: (x: any) => ({
