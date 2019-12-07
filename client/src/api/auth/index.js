7907c5c9fbaf5f5cbd917f3f0c2ecfc5eebf18cf
PiddePannkauga <petter.mansson1@gmail.com>
2019-12-07 16:39:50 +0100

    Add jwt to vuex store on login (#67)
    * Add jwt to vuex store on login
    
    * Save JWT in vuex store
    
    * WIP silent refresh
    
    * WIP save refreshToken
    
    * Add storage for JWT and API endpoint call for silentTokenRefresh
    
    * Fix format in store.js
    
    * Dont track .env files for local testing

--- client/src/api/auth/index.js
+++ client/src/api/auth/index.js
@@ -1,9 +1,23 @@
 import gql from 'graphql-tag';
+import axios from 'axios'
+
+var API = axios.create({
+    baseURL: process.env.VUE_APP_API_BASE_URL,
+    timeout: 30000,
+});
+
+export function refreshToken(){
+    return API.post('/api/refreshToken', {}, {withCredentials: true});
+}
+
+
 
 export const LOGIN = gql `
 mutation ($email: String!,$password: String!){
 	login(email: $email, password: $password) {
-		JWT
+        JWT
+        JWTExpiry
+        id
     }
 }`
 
--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,53 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {
+  refreshToken
+} from '@/api/auth';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    userID: "",
+    isLoggedin: false,
+    inMemoryToken: {
+      JWT: "",
+      JWTExpiry: ""
+    }
   },
   mutations: {
-
+    setUserID(state, value) {
+      state.userID = value;
+    },
+    setIsLoggedin(state, value) {
+      state.isLoggedin = value;
+    },
+    setInMemoryToken(state, {
+      JWT,
+      JWTExpiry
+    }) {
+      state.inMemoryToken.JWT = JWT;
+      state.inMemoryToken.JWTExpiry = JWTExpiry;
+    }
   },
   actions: {
+
+    silentTokenRefresh(context) { //eslint-disable-line
+      if (!this.state.inMemoryToken.JWT) {
+        return false;
+      } else {
+        refreshToken().then((response) => {
+          const {
+            JWT,
+            JWTExpiry
+          } = response.data;
+          context.commit('setInMemoryToken', {
+            JWT,
+            JWTExpiry
+          });
+        })
+      }
+    },
 
   }
 })
