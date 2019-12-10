c15cafea7f330e85e8278764c6a3551ec769f592
PiddePannkauga <petter.mansson1@gmail.com>
2019-12-10 21:50:58 +0100

    Add components for wateringschedule (#66)
    * Add components for wateringschedule
    
    * Fix incrementerbutton to only take number as valid input
    
    * Fix calendar showing todays date when opening view
    
    * Add ok/cancel button to view

---
+++ client/src/views/AddWateringSchedule.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <q-input label="Name the plant are you going to water" outlined standout />
+    <q-date v-model="date" :dark="true" today-btn color="green" />
+    <q-btn-group>
+      <q-btn label="Days" />
+      <q-btn label="Weeks" />
+    </q-btn-group>
+    <IncrementerButton />
+    <q-btn-group>
+      <q-btn label="Cancel" />
+      <q-btn label="Add schedule" />
+    </q-btn-group>
+  </div>
+</template>
+
+<script>
+import { QDate, QInput, QBtnGroup, QBtn } from "quasar";
+import IncrementerButton from "../components/IncrementerButton.vue";
+
+export default {
+  name: "AddWateringSchedule",
+  components: {
+    QDate,
+    QInput,
+    QBtnGroup,
+    QBtn,
+    IncrementerButton
+  },
+  data() {
+    return {
+      date: ""
+    };
+  },
+  created() {
+    this.date = new Date()
+      .toISOString()
+      .slice(0, 10)
+      .replace(/-/g, "/");
+  }
+};
+</script>
+
+<style scoped>
+</style>
