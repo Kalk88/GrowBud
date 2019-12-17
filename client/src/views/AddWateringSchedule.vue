bd4dc509268e18f1a05ef8d4b42bbb70606b598c
petter <petter.mansson1@gmail.com>
2019-12-17 21:29:13 +0100

    Add grid layout to wateringschedule

--- client/src/views/AddWateringSchedule.vue
+++ client/src/views/AddWateringSchedule.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <q-input label="Name the plant are you going to water" outlined standout />
-    <q-date v-model="date" :dark="true" today-btn color="green" />
-    <q-btn-group>
-      <q-btn label="Days" />
-      <q-btn label="Weeks" />
-    </q-btn-group>
-    <IncrementerButton />
-    <q-btn-group>
+  <div class="wrapper">
+    <q-input
+      class="plantName-input"
+      label="Name the plant that you are going to water"
+      outlined
+      standout
+    />
+    <q-date class="calendar" v-model="date" :dark="true" today-btn color="green" />
+    <div class="interval-selector">
+      <q-btn-group class="interval-btn-grp">
+        <q-btn label="Days" />
+        <q-btn label="Weeks" />
+      </q-btn-group>
+      <IncrementerButton class="incrementer-button" v-model="interval" />
+    </div>
+    <q-btn-group class="add-cancel-btn-grp">
       <q-btn label="Cancel" />
       <q-btn label="Add schedule" />
     </q-btn-group>
@@ -29,7 +36,10 @@ export default {
   },
   data() {
     return {
-      date: ""
+      date: "",
+      interval: 0,
+      intervalModifier: "DAYS",
+      plantName: ""
     };
   },
   created() {
@@ -42,4 +52,42 @@ export default {
 </script>
 
 <style scoped>
+.wrapper {
+  display: grid;
+  justify-items: center;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  grid-row-gap: 1rem;
+  margin-top: 3rem;
+}
+
+.plantName-input {
+  grid-column: span 4;
+  width: 50%;
+}
+
+.calendar {
+  justify-self: end;
+  align-self: center;
+  grid-column: 2;
+}
+
+.interval-btn-grp {
+  justify-self: end;
+  align-self: center;
+}
+
+.interval-selector {
+  grid-column: 3;
+  display: flex;
+}
+.incrementer-button {
+  justify-self: start;
+  align-self: center;
+}
+
+.add-cancel-btn-grp {
+  grid-column: 3;
+  grid-row: 3;
+}
 </style>
