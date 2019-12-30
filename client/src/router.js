96b5234032dc2700fc1d7c2d67a1a131d5ed189d
PiddePannkauga <petter.mansson1@gmail.com>
2019-12-30 14:40:27 +0100

    Add navbar for loggedin user (#82)

--- client/src/router.js
+++ client/src/router.js
@@ -4,31 +4,34 @@ import DefaultLayout from './layouts/Default.vue'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import AddWateringSchedule from './views/AddWateringSchedule.vue'
+import MyWateringSchedules from './views/MyWateringSchedules.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      component: DefaultLayout,
-      children: [
-        {
-          path: '',
-          name: 'home',
-          component: Home
-        },
-        {
-          path: '/about',
-          name: 'about',
-          component: About
-        },
-        {
-          path: '/addWateringSchedule',
-          name: 'addWateringSchedule',
-          component: AddWateringSchedule
-        }
-      ]
-    }
-  ]
+  routes: [{
+    path: '/',
+    component: DefaultLayout,
+    children: [{
+        path: '',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/about',
+        name: 'about',
+        component: About
+      },
+      {
+        path: '/addWateringSchedule',
+        name: 'addWateringSchedule',
+        component: AddWateringSchedule
+      },
+      {
+        path: '/myWateringSchedules',
+        name: 'myWateringSchedules',
+        component: MyWateringSchedules
+      }
+    ]
+  }]
 })
