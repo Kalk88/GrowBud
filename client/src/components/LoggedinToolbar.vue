a1ac6cbc7c45c541e7082da0a5711ba0476f3355
PiddePannkauga <petter.mansson1@gmail.com>
2019-12-30 14:40:27 +0100

    Add navbar for loggedin user (#82)

--- client/src/components/LoggedinToolbar.vue
+++ client/src/components/LoggedinToolbar.vue
@@ -1,15 +1,31 @@
 <template>
-    <q-toolbar>
+  <q-toolbar>
     <q-toolbar-title>Growbud</q-toolbar-title>
-    <div> Välkommen in </div>
-        
-    </q-toolbar>
+    <nav>
+      <div id="addSchedule" @click="$router.push('addWateringSchedule')">Add a schedule</div>
+      <div id="mySchedules" @click="$router.push('myWateringSchedules')">My schedules</div>
+    </nav>
+  </q-toolbar>
 </template>
 <script>
 export default {
-    name: "LoggedinToolbar"
-}
+  name: "LoggedinToolbar"
+};
 </script>
 <style scoped>
+nav {
+  display: flex;
+  flex-direction: row;
+}
 
+nav > div {
+  padding: 10px;
+}
+
+#addSchedule:hover {
+  color: black;
+}
+#mySchedules:hover {
+  color: black;
+}
 </style>
