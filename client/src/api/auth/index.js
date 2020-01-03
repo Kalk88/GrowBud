598a62e006d3b880f07c94228f3f2ec3bf5604ea
petter <petter.mansson1@gmail.com>
2020-01-03 14:22:11 +0100

    WIP

--- client/src/api/auth/index.js
+++ client/src/api/auth/index.js
@@ -1,13 +1,29 @@
 import gql from 'graphql-tag';
 import axios from 'axios'
+import store from './store'
 
 var API = axios.create({
     baseURL: process.env.VUE_APP_API_BASE_URL,
     timeout: 30000,
 });
 
-export function refreshToken(){
-    return API.post('/api/refreshToken', {}, {withCredentials: true});
+API.interceptors.response.use((response) => {
+    if (response.status === 401) {
+        store.dispatch('silentTokenRefresh');
+    }
+    return response;
+}, (error) => {
+    if (error.response && error.response.data) {
+        this.$router.push('home');
+        return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error.message);
+})
+
+export function refreshToken() {
+    return API.post('/api/refreshToken', {}, {
+        withCredentials: true
+    });
 }
 
 
