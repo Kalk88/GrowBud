322ec3063d345ce22f359b72ec970f20cc40c975
Simon Månsson <simon.d.mansson@gmail.com>
2020-01-06 23:34:53 +0100

    Refactor login function (#94)
    * Refactor login function
    
    Refactor login function to remove local state variables and use
    expires to milliseconds function.
    
    * remove unused import

--- server/data-store/src/lib/auth/index.ts
+++ server/data-store/src/lib/auth/index.ts
@@ -19,35 +19,29 @@ export interface RefreshInfo {
   refreshToken: string
 }
 
-export async function login(credential: Credential): Promise<userInfo> {
-  try {
-    const { email, password } = credential
-    const payload = {
-      email,
-      password,
+export function login(credential: Credential): Promise<userInfo> {
+  return axios({
+    method: "POST",
+    headers: { 'content-type': 'application/json' },
+    data: {
+      email: credential.email,
+      password: credential.password,
       returnSecureToken: true
-    }
-    const response: any = await axios({
-      method: "POST",
-      headers: { 'content-type': 'application/json' },
-      data: payload,
-      url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASEAPIKEY}`,
+    },
+    url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.FIREBASEAPIKEY}`,
+  })
+    .then(response => response.data)
+    .then(data => {
+      return {
+        id: data.localId,
+        JWT: data.idToken,
+        JWTExpiry: expiresToMilliseconds(data.expiresIn),
+        refreshToken: data.refreshToken
+      }
+    }).catch(error => {
+      console.error('Unable to authorize user: ', error.toJSON())
+      throw (new Error('Authentication error'))
     })
-
-    const { localId, idToken, expiresIn, refreshToken } = response.data
-    // Date.now() is milliseconds, expiresIn is seconds.
-    const JWTExpiry = Date.now() + (expiresIn * 1000)
-    return {
-      id: localId,
-      JWT: idToken,
-      JWTExpiry,
-      refreshToken: refreshToken
-    }
-
-  } catch (error) {
-    console.error('Unable to authorize user: ', error.toJSON())
-    throw (new Error('Authentication error'))
-  }
 }
 
 export async function registerUser(credential: Credential, userName: string): Promise<boolean> {
@@ -82,7 +76,7 @@ export function verifyAndDecodeToken(token: string): Promise<object> {
   return admin.auth().verifyIdToken(token)
 }
 
-export async function refreshToken(token: string): Promise<RefreshInfo> {
+export function refreshToken(token: string): Promise<RefreshInfo> {
   return axios({
     method: "POST",
     headers: { 'content-type': 'application/x-www-form-urlencoded' },
