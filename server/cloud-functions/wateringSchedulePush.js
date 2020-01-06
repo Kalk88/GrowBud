9f733ef24fc23a6c2dc18d16e0df67eb1d5921b1
Kalk <simon.d.mansson@gmail.com>
2020-01-06 22:22:12 +0100

    Refactor cloud functions

--- server/cloud-functions/wateringSchedulePush.js
+++ server/cloud-functions/wateringSchedulePush.js
@@ -1,17 +1,16 @@
 const admin = require('firebase-admin')
 const app = admin.initializeApp()
+const messaging = app.messaging()
 
 /**
  * Expects messages to be an array and each entry to conform to the FCM message spec.
  */
-exports.pushWateringScheduleNotice = async (pubSubEvent, context) => {
-    const wateringNotices = JSON.parse(Buffer.from(pubSubEvent.data, 'base64').toString())
-    console.log(wateringNotices)
-    try {
-        const response = await app.messaging().sendAll(wateringNotices)
-        console.log('Successfully sent message:', response)
-
-    } catch (error) {
-        console.log('Error sending message:', error)
-    }
+exports.pushWateringScheduleNotice = (pubSubEvent, context) => {
+    [pubSubEvent.data]
+        .map(msg => Buffer.from(msg, 'base64'))
+        .map(JSON.parse)
+        .map(toString)
+        .map(messaging.sendAll)
+        .then(console.log)
+        .catch(error => console.error(error))
 }
