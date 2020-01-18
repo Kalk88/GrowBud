ebbe5dbbf28d3aeb02bdd75aec7e23f1417ccb9d
PiddePannkauga <petter.mansson1@gmail.com>
2020-01-18 11:08:39 +0100

    Install vue PWA plugin (#77)
    * Instal vue PWA plugin
    
    *  Update pwa plugin
    
    * Refactor vue.config.js
    
    * Add service worker for push notification

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-  pluginOptions: {
-    quasar: {
-      treeShake: true
-    }
-  },
-  transpileDependencies: [
-    /[\\/]node_modules[\\/]quasar[\\/]/,
-  ]
-}
+    pwa: {
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+          swSrc: 'src/firebase-messaging-sw.js',
+          swDest: 'firebase-messaging-sw.js'
+        }
+    },
+
+    pluginOptions: {
+      quasar: {
+        treeShake: true
+      }
+    },
+    transpileDependencies: [
+      /[\\/]node_modules[\\/]quasar[\\/]/,
+    ]
+
+  }
+  
