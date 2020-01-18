79aabbf9a6b2d7aa24ff241563ac8976dab0add4
Simon Månsson <simon.d.mansson@gmail.com>
2020-01-18 10:56:30 +0100

    Fix access control allow origin header (#98)
    The origin header may only contain one value so we have to match it
    with the white list.

--- server/data-store/src/index.ts
+++ server/data-store/src/index.ts
@@ -6,12 +6,14 @@ import { refreshToken as rf, RefreshInfo } from './lib/auth'
 import cookieParser from 'cookie-parser'
 const app = express()
 const port = process.env.PORT ? process.env.PORT : 9090
-const whitelist = process.env.WHITELIST
+const whitelist = process.env.WHITELIST ? process.env.WHITELIST.split(',') : []
 
 app.use(express.json())
 app.use(cookieParser())
-app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', whitelist)
+app.use((req, res, next) => {
+  whitelist.forEach(host => {
+    if (req.headers.origin === host) res.header('Access-Control-Allow-Origin', req.headers.origin)
+  })
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   res.header('Access-Control-Allow-Methods', 'GET, POST')
   res.header('Access-Control-Allow-Credentials', 'true')
