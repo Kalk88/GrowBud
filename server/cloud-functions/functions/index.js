8b1236f0470ea0d6d3542e03ab021ec4280b9b6e
Kalk <simon.d.mansson@gmail.com>
2020-01-31 19:52:24 +0100

    Send push on to found schedules

--- server/cloud-functions/functions/index.js
+++ server/cloud-functions/functions/index.js
@@ -3,24 +3,38 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 const messaging = admin.messaging()
 const firestore = admin.firestore()
-
+/**
+ * hitta scheman
+ * gruppera på användare
+ *
+ * för varje användare
+ *  hämta tokens
+ *  skicka till alla devices
+ *
+ * updatera schema
+ */
 exports.notifySchedulesInRange = functions.region('europe-west1').https.onRequest((req, res) => {
     retrieveSchedules(`${Date.now() - (60 * 60 * 24)}`, `${Date.now() + (60 * 60 * 24)}`)
         .then(snapshot => {
             console.log('docs', JSON.stringify(snapshot.docs))
             return snapshot.docs
                 .map(doc => doc.data())
-                .reduce(reduceSchedules, {})
-                .map(doc => Object.entries(doc).map((k, v) =>
-                    retrieveNotifications(k)
-                        .then(tokens => {
-                            console.log('tokens', JSON.stringify(tokens.data()))
-                            return messaging.sendAll([tokens.data()].map(d => formatMessage(v, d.deviceToken))).then(console.log).catch(console.log)
-                        }).catch(err => reject(err))
-                ))
+                .reduce(reduceSchedulesOnUserId, {})
+        })
+        .then(doc => {
+            console.log('then doc', JSON.stringify(doc))
+            Object.entries(doc).map(([k, v]) => {
+                console.log('key', k)
+                retrieveNotifications(k)
+                    .then(tokens => {
+                        console.log('tokens', JSON.stringify(tokens.data()))
+                        return messaging.sendAll([tokens.data()].map(d => formatMessage(v, d.deviceToken))).then(console.log).catch(console.log)
+                    }).catch(err => reject(err))
+            })
         })
         .then(it => { console.log(it) })
         .then(res.status(200).send('finished'))
+        .catch(err => console.log(err.message))
 })
 /**
  * Expects messages to be an array and each entry to conform to the FCM message spec.
@@ -42,7 +56,8 @@ const retrieveNotifications = userId => firestore.collection('pushNotifications'
     .doc(userId)
     .get()
 
-const reduceSchedules = (accumulator, current) => {
+const reduceSchedulesOnUserId = (accumulator, current) => {
+    console.log('current', JSON.stringify(current))
     if (accumulator.hasOwnProperty(current.userId)) {
         accumulator[current.userId].push(current)
         return accumulator
@@ -53,4 +68,6 @@ const reduceSchedules = (accumulator, current) => {
     }
 }
 
-//exports.reduceSchedules = reduceSchedules
+const asPromised = (f) => new Promise((resolve, reject) => resolve(f()))
+
+exports.reduceSchedulesOnUserId = reduceSchedulesOnUserId
