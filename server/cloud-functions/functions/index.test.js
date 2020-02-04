d4100e82638aed61df669e429dc0f18a4de9aee2
Kalk <simon.d.mansson@gmail.com>
2020-02-04 19:09:28 +0100

    Wip

--- server/cloud-functions/functions/index.test.js
+++ server/cloud-functions/functions/index.test.js
@@ -2,20 +2,20 @@ const test = require('ava')
 const f = require('./index.js')
 
 test('reduce multiple schedules on userId', t => {
-    const schedules = [
-        {
-            userId: 'bob',
-            plants: [2]
-        },
-        {
-            userId: 'lisa',
-            plants: [1]
-        },
-        {
-            userId: 'bob',
-            plants: [1]
-        },
-    ]
-    const res = schedules.reduce(f.reduceSchedulesOnUserId, {})
-    t.is(2, res.bob.length)
+  const schedules = [
+    {
+      userId: 'bob',
+      plants: [2]
+    },
+    {
+      userId: 'lisa',
+      plants: [1]
+    },
+    {
+      userId: 'bob',
+      plants: [1]
+    }
+  ]
+  const res = schedules.reduce(f.reduceSchedulesOnUserId, {})
+  t.is(2, res.bob.length)
 })
