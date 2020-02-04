8f3ce23b0cccb9845f7b887b8dcc792935d8c2a6
Kalk <simon.d.mansson@gmail.com>
2020-02-04 19:09:28 +0100

    Wip

--- server/cloud-functions/functions/index.js
+++ server/cloud-functions/functions/index.js
@@ -1,73 +1,82 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp()
 const messaging = admin.messaging()
 const firestore = admin.firestore()
-/**
- * hitta scheman
- * gruppera på användare
- *
- * för varje användare
- *  hämta tokens
- *  skicka till alla devices
- *
- * updatera schema
- */
+
 exports.notifySchedulesInRange = functions.region('europe-west1').https.onRequest((req, res) => {
-    retrieveSchedules(`${Date.now() - (60 * 60 * 24)}`, `${Date.now() + (60 * 60 * 24)}`)
-        .then(snapshot => {
-            console.log('docs', JSON.stringify(snapshot.docs))
-            return snapshot.docs
-                .map(doc => doc.data())
-                .reduce(reduceSchedulesOnUserId, {})
-        })
-        .then(doc => {
-            console.log('then doc', JSON.stringify(doc))
-            Object.entries(doc).map(([k, v]) => {
-                console.log('key', k)
-                retrieveNotifications(k)
-                    .then(tokens => {
-                        console.log('tokens', JSON.stringify(tokens.data()))
-                        return messaging.sendAll([tokens.data()].map(d => formatMessage(v, d.deviceToken))).then(console.log).catch(console.log)
-                    }).catch(err => reject(err))
-            })
-        })
-        .then(it => { console.log(it) })
-        .then(res.status(200).send('finished'))
-        .catch(err => console.log(err.message))
+  retrieveSchedules(`${Date.now()}`)
+    .then(snapshot => {
+      console.log('docs', JSON.stringify(snapshot.docs))
+      return snapshot.docs
+        .map(doc => ({
+          id: doc.id,
+          schedule: doc.data()
+        }))
+        .reduce(reduceSchedulesOnUserId, {})
+    })
+    .then(doc => {
+      console.log('then doc', JSON.stringify(doc))
+      Object.entries(doc).map(([userId, v]) => {
+        retrieveDeviceTokens(userId)
+          .then(tokens => {
+            console.log('tokens', JSON.stringify(tokens.data()))
+            console.log('V', JSON.stringify(v))
+            return messaging.sendAll(
+              [tokens.data()].map(d => formatMessage(v, d.deviceToken))
+            )
+              .then(() => {
+                const batch = firestore.batch()
+                v.forEach(schedule => {
+                  const ref = firestore.collection('wateringSchedules').doc(schedule.docId)
+                  batch.set(ref, { nextTimeToWater: schedule.nextTimeToWater + 60 * 60 * 24 * schedule.interval })
+                })
+                return batch.commit().then(console.log).catch(console.log)
+              })
+              .catch(err => {
+                console.log(
+                  {
+                    error: 'push notification error',
+                    message: err.message
+                  })
+              })
+          })
+      })
+    })
+    .then(it => { console.log(JSON.stringify(it)) })
+    .then(res.status(200).send('finished'))
+    .catch(err => console.log(err.message))
 })
-/**
- * Expects messages to be an array and each entry to conform to the FCM message spec.
- */
-const formatMessage = (doc, token) => ({
+
+const formatMessage = (doc, token) => {
+  console.log(JSON.stringify(doc))
+  return {
     token,
     notification: {
-        title: 'Time to water!',
-        body: `${doc.map(item => item.plants.map(plant => plant.name).join(', '))} needs watering.`,
+      title: 'Time to water!',
+      body: `${doc.map(item => item.plants.map(plant => plant.name).join(', '))} needs watering.`
     }
-})
-
-const retrieveSchedules = (past, future) => firestore.collection('wateringSchedules')
-    .where('nextTimeToWater', '>', past)
-    //.where('NextTimeToWater', '<', future)
-    .get()
-
-const retrieveNotifications = userId => firestore.collection('pushNotifications')
-    .doc(userId)
-    .get()
-
-const reduceSchedulesOnUserId = (accumulator, current) => {
-    console.log('current', JSON.stringify(current))
-    if (accumulator.hasOwnProperty(current.userId)) {
-        accumulator[current.userId].push(current)
-        return accumulator
-    }
-    return {
-        ...accumulator,
-        [current.userId]: [current]
-    }
+  }
 }
 
-const asPromised = (f) => new Promise((resolve, reject) => resolve(f()))
+const retrieveSchedules = time => firestore.collection('wateringSchedules')
+  .where('nextTimeToWater', '<', time)
+  .get()
+
+const retrieveDeviceTokens = userId => firestore.collection('pushNotifications')
+  .doc(userId)
+  .get()
+
+const reduceSchedulesOnUserId = (accumulator, current) => {
+  console.log('current', JSON.stringify(current))
+  if (accumulator[current.schedule.userId]) {
+    accumulator[current.schedule.userId].push(current)
+    return accumulator
+  }
+  return {
+    ...accumulator,
+    [current.schedule.userId]: [current]
+  }
+}
 
 exports.reduceSchedulesOnUserId = reduceSchedulesOnUserId
