294145210b4137d0104a06d2ab89671a1f6054ec
Kalk <simon.d.mansson@gmail.com>
2020-02-05 22:54:57 +0100

    working

--- server/cloud-functions/functions/index.js
+++ server/cloud-functions/functions/index.js
@@ -3,11 +3,71 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 const messaging = admin.messaging()
 const firestore = admin.firestore()
-
+/*
+exports.notifySchedulesInRange = functions.region('europe-west1').https.onRequest(async (req, res) => {
+  const schedules = await retrieveSchedules(`${Date.now()}`)
+  console.log(`Found ${schedules.docs.length} schedules to update`)
+  const orderedOnUserId = schedules.docs
+    .map(doc => ({
+      id: doc.id,
+      schedule: doc.data()
+    }))
+    .reduce(reduceSchedulesOnUserId, {})
+  console.log('ordered on userId', JSON.stringify(orderedOnUserId))
+  let tokensAndSchedules = []
+  /*
+  try {
+    const res = await Promise.all(Object.keys(orderedOnUserId).map(async k => {
+      const tokens = await retrieveDeviceTokens(k)
+      console.log('bruv', JSON.stringify(tokens.data()))
+      return {
+        k: tokens.data()
+      }
+    }))
+    console.log('post bruv', JSON.stringify(res))
+  } catch (error) {
+    console.log(JSON.stringify(error))
+  }
+  */
+/*
+  try {
+    tokensAndSchedules = await Promise.all(Object.entries(orderedOnUserId).map(async ([k, v]) => {
+      const tokens = await retrieveDeviceTokens(k)
+      console.log('tokens', JSON.stringify(tokens.data()))
+      if (tokens.exists) {
+        return {
+          [k]: {
+            ...v,
+            tokens: [tokens.data()]
+          }
+        }
+      } else {
+        return {
+          [k]: {
+            ...v,
+            tokens: []
+          }
+        }
+      }
+    }))
+  } catch (error) {
+    console.log(JSON.stringify(error))
+  }
+  console.log('post tokens', JSON.stringify(tokensAndSchedules))
+  const formated = tokensAndSchedules.map(o => o.tokens.map(t => formatMessage(o.schedule, t.deviceToken)))
+  try {
+    const res = await messaging.sendAll(formated)
+    console.log(JSON.stringify(res))
+  } catch (error) {
+    console.log(JSON.stringify(error))
+  }
+  res.status(200).send('finished')
+})
+*/
 exports.notifySchedulesInRange = functions.region('europe-west1').https.onRequest((req, res) => {
-  retrieveSchedules(`${Date.now()}`)
+  return retrieveSchedules(`${Date.now()}`)
     .then(snapshot => {
-      console.log('docs', JSON.stringify(snapshot.docs))
+      console.log(`Found ${snapshot.docs.length} schedules to update`)
       return snapshot.docs
         .map(doc => ({
           id: doc.id,
@@ -15,48 +75,74 @@ exports.notifySchedulesInRange = functions.region('europe-west1').https.onReques
         }))
         .reduce(reduceSchedulesOnUserId, {})
     })
-    .then(doc => {
-      console.log('then doc', JSON.stringify(doc))
-      Object.entries(doc).map(([userId, v]) => {
+    .then(reduced => {
+      console.log('docs per user: ', JSON.stringify(reduced))
+      return Promise.all(Object.entries(reduced).map(([userId, data]) => {
+        console.log(userId, JSON.stringify(data))
         return retrieveDeviceTokens(userId)
           .then(tokens => {
+            console.log('tokens', JSON.stringify(tokens.data()))
+            console.log('data', JSON.stringify(data))
             if (tokens.exists) {
-              console.log('tokens', JSON.stringify(tokens.data()))
-              console.log('V', JSON.stringify(v))
-              return messaging.sendAll(
-                [tokens.data()].map(d => formatMessage(v, d.deviceToken)).then(() => v)).catch(err => console.log(JSON.stringify(err)))
+              return {
+                [userId]: {
+                  ...data,
+                  tokens: [tokens.data()]
+                }
+              }
             }
-            return new Promise((resolve, reject) => resolve(v))
-          })
-      })
-        .then(v => {
-          const batch = firestore.batch()
-          v.forEach(schedule => {
-            const ref = firestore.collection('wateringSchedules').doc(schedule.docId)
-            batch.set(ref, { nextTimeToWater: schedule.nextTimeToWater + 60 * 60 * 24 * schedule.interval })
-          })
-          return batch.commit().then(console.log).catch(console.log)
-        })
-        .catch(err => {
-          console.log(
-            {
-              error: 'push notification error',
-              message: err.message
-            })
-        })
+            return {
+              [userId]: {
+                ...data,
+                tokens: []
+              }
+            }
+          }).catch(error => console.log(JSON.stringify(error)))
+      }))
     })
-    .then(it => { console.log(JSON.stringify(it)) })
-    .then(res.status(200).send('finished'))
-    .catch(err => console.log(err.message))
+    .then(data => {
+      console.log('data to send', JSON.stringify(data))
+      return Promise.all(data.map(obj => {
+        const messages = Object.entries(obj).map(([k, v]) => v.tokens.map(d => formatMessage(v.schedules, d.deviceToken)))[0]
+        console.log('messages', JSON.stringify(messages))
+        if (messages.length > 0) {
+          return messaging.sendAll(messages)
+            .catch(err => console.log(JSON.stringify(err)))
+        }
+      })).then(() => data).finally(() => data)
+    })
+    .then(v => {
+      console.log('batch writing: ', JSON.stringify(v))
+      return Promise.all(v.map(schedule => {
+        return Object.entries(schedule).map(([k, x]) => {
+          console.log('x', JSON.stringify(x))
+          return x.schedules.map(s => {
+            console.log('s', JSON.stringify(s))
+            const toSave = s.schedule
+            toSave.nextTimeToWater = (parseInt(s.schedule.nextTimeToWater) + (86400 * s.schedule.interval)).toString()
+            return firestore.collection('wateringSchedules').doc(s.id).set(toSave)
+          })
+        })
+      })).then(status => status).finally(status => status)
+    })
+    .then(status => res.status(200).send(status))
+    .catch(err => {
+      console.log(JSON.stringify(
+        {
+          error: 'push notification error',
+          message: err.message
+        })
+      )
+    })
 })
 
 const formatMessage = (doc, token) => {
-  console.log(JSON.stringify(doc))
+  console.log('formatting', JSON.stringify(doc))
   return {
     token,
     notification: {
       title: 'Time to water!',
-      body: `${doc.map(item => item.plants.map(plant => plant.name).join(', '))} needs watering.`
+      body: `${doc.map(item => item.schedule.plants.map(plant => plant.name).join(', '))} needs watering.`
     }
   }
 }
@@ -70,14 +156,15 @@ const retrieveDeviceTokens = userId => firestore.collection('pushNotifications')
   .get()
 
 const reduceSchedulesOnUserId = (accumulator, current) => {
-  console.log('current', JSON.stringify(current))
   if (accumulator[current.schedule.userId]) {
-    accumulator[current.schedule.userId].push(current)
+    accumulator[current.schedule.userId].schedules.push(current)
     return accumulator
   }
   return {
     ...accumulator,
-    [current.schedule.userId]: [current]
+    [current.schedule.userId]: {
+      schedules: [current]
+    }
   }
 }
 
--- server/cloud-functions/functions/index.test.js
+++ server/cloud-functions/functions/index.test.js
@@ -4,18 +4,62 @@ const f = require('./index.js')
 test('reduce multiple schedules on userId', t => {
   const schedules = [
     {
-      userId: 'bob',
-      plants: [2]
+      id: 'asd',
+      schedule: {
+        userId: 'bob',
+        plants: [2]
+      }
     },
     {
-      userId: 'lisa',
-      plants: [1]
+      id: 'dd',
+      schedule: {
+        userId: 'lisa',
+        plants: [1]
+      }
     },
     {
-      userId: 'bob',
-      plants: [1]
+      id: 'xx',
+      schedule: {
+        userId: 'bob',
+        plants: [1]
+      }
     }
   ]
   const res = schedules.reduce(f.reduceSchedulesOnUserId, {})
   t.is(2, res.bob.length)
+})
+
+test('a', t => {
+  const res = new Promise((resolve, reject) => resolve(
+    {
+      a: 'b',
+      c: 'd'
+    }))
+  return res.then(o => {
+    return new Promise((resolve, reject) => {
+      resolve(Object.entries(o)
+        .map(([k, v]) => {
+          console.log(k, v)
+          return {
+            [k]: { [v]: [] }
+          }
+        })
+      )
+    })
+  })
+    .then(result => {
+      console.log(result)
+      const expected = [
+        {
+          a: {
+            b: []
+          }
+        }, {
+          c: {
+            d: []
+          }
+        }
+      ]
+      t.deepEqual(result, expected)
+    })
 })
