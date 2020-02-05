cf7f6eb330f9e306a6e247cfef76b7624a9c7fc2
Kalk <simon.d.mansson@gmail.com>
2020-02-05 22:55:06 +0100

    remove console logs

--- server/cloud-functions/functions/index.js
+++ server/cloud-functions/functions/index.js
@@ -3,67 +3,7 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 const messaging = admin.messaging()
 const firestore = admin.firestore()
-/*
-exports.notifySchedulesInRange = functions.region('europe-west1').https.onRequest(async (req, res) => {
-  const schedules = await retrieveSchedules(`${Date.now()}`)
-  console.log(`Found ${schedules.docs.length} schedules to update`)
-  const orderedOnUserId = schedules.docs
-    .map(doc => ({
-      id: doc.id,
-      schedule: doc.data()
-    }))
-    .reduce(reduceSchedulesOnUserId, {})
-  console.log('ordered on userId', JSON.stringify(orderedOnUserId))
-  let tokensAndSchedules = []
-  /*
-  try {
-    const res = await Promise.all(Object.keys(orderedOnUserId).map(async k => {
-      const tokens = await retrieveDeviceTokens(k)
-      console.log('bruv', JSON.stringify(tokens.data()))
-      return {
-        k: tokens.data()
-      }
-    }))
-    console.log('post bruv', JSON.stringify(res))
-  } catch (error) {
-    console.log(JSON.stringify(error))
-  }
-  */
-/*
-  try {
-    tokensAndSchedules = await Promise.all(Object.entries(orderedOnUserId).map(async ([k, v]) => {
-      const tokens = await retrieveDeviceTokens(k)
-      console.log('tokens', JSON.stringify(tokens.data()))
-      if (tokens.exists) {
-        return {
-          [k]: {
-            ...v,
-            tokens: [tokens.data()]
-          }
-        }
-      } else {
-        return {
-          [k]: {
-            ...v,
-            tokens: []
-          }
-        }
-      }
-    }))
-  } catch (error) {
-    console.log(JSON.stringify(error))
-  }
-  console.log('post tokens', JSON.stringify(tokensAndSchedules))
-  const formated = tokensAndSchedules.map(o => o.tokens.map(t => formatMessage(o.schedule, t.deviceToken)))
-  try {
-    const res = await messaging.sendAll(formated)
-    console.log(JSON.stringify(res))
-  } catch (error) {
-    console.log(JSON.stringify(error))
-  }
-  res.status(200).send('finished')
-})
-*/
+
 exports.notifySchedulesInRange = functions.region('europe-west1').https.onRequest((req, res) => {
   return retrieveSchedules(`${Date.now()}`)
     .then(snapshot => {
@@ -76,13 +16,9 @@ exports.notifySchedulesInRange = functions.region('europe-west1').https.onReques
         .reduce(reduceSchedulesOnUserId, {})
     })
     .then(reduced => {
-      console.log('docs per user: ', JSON.stringify(reduced))
       return Promise.all(Object.entries(reduced).map(([userId, data]) => {
-        console.log(userId, JSON.stringify(data))
         return retrieveDeviceTokens(userId)
           .then(tokens => {
-            console.log('tokens', JSON.stringify(tokens.data()))
-            console.log('data', JSON.stringify(data))
             if (tokens.exists) {
               return {
                 [userId]: {
@@ -101,10 +37,8 @@ exports.notifySchedulesInRange = functions.region('europe-west1').https.onReques
       }))
     })
     .then(data => {
-      console.log('data to send', JSON.stringify(data))
       return Promise.all(data.map(obj => {
         const messages = Object.entries(obj).map(([k, v]) => v.tokens.map(d => formatMessage(v.schedules, d.deviceToken)))[0]
-        console.log('messages', JSON.stringify(messages))
         if (messages.length > 0) {
           return messaging.sendAll(messages)
             .catch(err => console.log(JSON.stringify(err)))
@@ -112,12 +46,9 @@ exports.notifySchedulesInRange = functions.region('europe-west1').https.onReques
       })).then(() => data).finally(() => data)
     })
     .then(v => {
-      console.log('batch writing: ', JSON.stringify(v))
       return Promise.all(v.map(schedule => {
         return Object.entries(schedule).map(([k, x]) => {
-          console.log('x', JSON.stringify(x))
           return x.schedules.map(s => {
-            console.log('s', JSON.stringify(s))
             const toSave = s.schedule
             toSave.nextTimeToWater = (parseInt(s.schedule.nextTimeToWater) + (86400 * s.schedule.interval)).toString()
             return firestore.collection('wateringSchedules').doc(s.id).set(toSave)
@@ -137,7 +68,6 @@ exports.notifySchedulesInRange = functions.region('europe-west1').https.onReques
 })
 
 const formatMessage = (doc, token) => {
-  console.log('formatting', JSON.stringify(doc))
   return {
     token,
     notification: {
