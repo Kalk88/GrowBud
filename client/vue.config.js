8231b52da720807f273dd5f8e37050ea32b6f3d3
Kalk <simon.d.mansson@gmail.com>
2020-02-12 20:42:35 +0100

    refactor: 💡 Move firebase service worker to public folder
    Move the firebase service worker to public so it may be bundled without
    being registered in vue.config.js
    
    ✅ Closes: #120

--- client/vue.config.js
+++ client/vue.config.js
@@ -9,8 +9,6 @@ module.exports = {
   pwa: {
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
-      swSrc: "src/firebase-messaging-sw.js",
-      swDest: "firebase-messaging-sw.js"
     }
   }
 };
