9b68d5f587e7e74228125ac3dcb0dfe30fd8fdff
petter <petter.mansson1@gmail.com>
2020-02-12 19:08:35 +0100

    refactor: 💡 change start_url in manifest.json

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>OOOGABOOGA</div>
+  <div v-if="showInstallPromotion">Hej vill du installera</div>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
 export default {
-  name: "PageHome"
+  name: "PageHome",
+
+  data() {
+    return {
+      deferredPrompt: null,
+      showInstallPromotion: false
+    };
+  },
+
+  created() {
+    window.addEventListener("beforeinstallprompt ", e => {
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+      this.showInstallPrompt();
+    });
+  },
+
+  methods: {
+    showInstallPromotion() {
+      this.showInstallPromotion = true;
+    }
+  }
 };
 </script>
