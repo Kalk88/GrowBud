ef6fd0b90422a9467677d41481de6be984d1a81b
Kalk <simon.d.mansson@gmail.com>
2020-02-12 22:05:57 +0100

    feat: 🎸 Register device token on messaging getToken
    Post a device token to the backend when firebase returns it.

--- client/src/api/notifications/index.js
+++ client/src/api/notifications/index.js
@@ -1,6 +1,6 @@
 import * as firebase from "firebase/app";
 import "firebase/messaging"
-
+import gql from 'graphql-tag';
 
 const firebaseConfig = {
     apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
@@ -12,7 +12,7 @@ const firebaseConfig = {
     appId: process.env.VUE_APP_FIREBASE_APPID,
     measurementId: process.env.VUE_APP_FIREBASE_MEASEUREMENTID
 };
-  
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // Retrieve Firebase Messaging object.
@@ -20,8 +20,37 @@ const messaging = firebase.messaging();
 // Add the public key generated from the console here.
 messaging.usePublicVapidKey(process.env.VUE_APP_FIREBASE_PUBLICVAPIDKEY);
 //eslint-disable-nextline
-messaging.getToken().then(token => console.log("permission token: ", token))//eslint-disable-line
+messaging.getToken().then(token =>
+    postDeviceToken(navigator.userAgent, token)
+)
 
 messaging.onMessage((payload) => {
-    console.log("Push notification recieved: ", payload)//eslint-disable-line
+    console.log("Push notification received: ", payload)//eslint-disable-line
 })
+
+/**
+ *  Posts a device token to the backend.
+ * @param {string} deviceName
+ * @param {string} deviceToken
+ */
+function postDeviceToken(deviceName, deviceToken) {
+    const upsertDeviceToken = gql`
+        mutation upsertDeviceToken($name: String, $token: String)
+        upsertDeviceToken(deviceName: $name, deviceToken: $token) {
+        status
+    }
+    `
+    fetch(process.env.VUE_APP_API_BASE_URL + '/graph', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          upsertDeviceToken,
+          variables: { deviceName, deviceToken },
+        }),
+      })
+        .then(r => r.json())
+        .then(data => console.log('data returned:', data)); //eslint-disable-line
+}
