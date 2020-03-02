08a10fca8f21c068bfca14922f11c06bc60c49ca
Kalk <simon.d.mansson@gmail.com>
2020-03-02 19:08:57 +0100

    feat(device tokens): add delete device token route
    Change datastructure in firebase to allow for deletion of specific devices.

---
+++ server/data-store/express-helpers.ts
@@ -0,0 +1,10 @@
+import { response } from "express"
+
+// Int => Object => Object => Object
+const sendResponse = statusCode => body => response => response.status(statusCode).send(body)
+export const noContentResponse = response => response.sendStatusCode(204)
+export const badRequestResponse = sendResponse(400)
+export const internalServerErrorResponse = sendResponse(500)
+export const createdResponse = sendResponse(201)
+export const okResponse = sendResponse(200)
+
--- server/data-store/src/lib/pushNotifcationToken/index.ts
+++ server/data-store/src/lib/pushNotifcationToken/index.ts
@@ -1,4 +1,6 @@
 import { db, admin } from '../firebaseClient'
+import * as log from '../../logging'
+
 
 export interface PushNotificationToken {
     deviceToken: string
@@ -8,7 +10,7 @@ export interface PushNotificationToken {
 
 const COLLECTION = 'pushNotifications'
 
-export function upsertDeviceToken(userId: string, deviceToken: string, deviceName: string, createdAt: string): Promise<PushNotificationToken | Error> {
+export function insertDeviceToken(userId: string, deviceToken: string, deviceName: string, createdAt: string): Promise<PushNotificationToken | Error> {
     const data = {
                 deviceName,
                 deviceToken,
@@ -17,16 +19,20 @@ export function upsertDeviceToken(userId: string, deviceToken: string, deviceNam
 
     return db.collection(COLLECTION)
         .doc(userId)
-        .update({devices: admin.firestore.FieldValue.arrayUnion(data)})
+        .set({[deviceToken]: [data]})
         .then(_ => data)
-        .catch(() => {
-            return db.collection(COLLECTION)
-            .doc(userId)
-            .set({devices: [data]})
-            .then(_ => data)
-                .catch(error =>{
-                    console.log(error)
-                    return new Error()
-                })
+            .catch(error =>{
+                log.error('Could not insert device token', error)
+                return new Error()
+        })
+}
+
+export function removeDeviceToken(userId: string, deviceToken: string):Promise<boolean> {
+    return db.collection(COLLECTION)
+        .doc(userId).update({[deviceToken]: admin.firestore.FieldValue.delete()})
+        .then(_ => true)
+        .catch(error => {
+            log.error('Could not remove device token', error)
+            return false
         })
 }
