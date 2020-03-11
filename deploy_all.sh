6f1835f0aa258d39eab26920918bd6e78af759a2
Kalk <simon.d.mansson@gmail.com>
2020-03-11 21:57:13 +0100

    feat: 🎸 add deploy all script
    Add script for deploying all projects to the given environment

---
+++ deploy_all.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PROJECT_ID=$1
+
+echo "Deploying all projects to $PROJECT_ID abort now if unsure!..."
+
+#Deploy data-store
+echo "Deploying data-store"
+cd server/data-store
+echo "$(pwd)"
+npm install
+npm run build
+cp example.app.yaml $PROJECT_ID.app.yaml
+ sed -i "s/fbapikey/$FIREBASEAPIKEY/g" app.default.yaml
+ sed -i "s/prjectId/$PROJECT_ID/g" app.default.yaml
+echo y | gcloud app deploy $PROJECT_ID.app.yaml --project=$PROJECT_ID --promote
+rm $PROJECT_ID.app.yaml
+cd ../../
+
+#Deploy client
+echo "Deploying client"
+cd client
+echo "$(pwd)"
+npm install
+npm run build
+echo y | firebase deploy --only hosting:$PROJECT_ID
+cd ../
+
+#Deploy cloud-functions
+echo "Deploying cloud functions"
+cd server/cloud-functions/functions
+echo "$(pwd)"
+npm install
+npm run build
+echo y | firebase deploy --only functions --project=$PROJECT_ID
+cd ../../../
