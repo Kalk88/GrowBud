cde4c9cc8d601736008d3549a52cf3a875d7cfe9
Kalk <simon.d.mansson@gmail.com>
2020-03-20 23:28:56 +0100

    refactor: make logging functional
    #145

--- server/data-store/src/logging/index.ts
+++ server/data-store/src/logging/index.ts
@@ -5,24 +5,29 @@
 import log from 'loglevel'
 log.setDefaultLevel(process.env.LOG_LEVEL ? process.env.LOG_LEVEL as log.LogLevelDesc : 'info')
 
-function trace(...args: any[]): void {
+function trace(...args: any[]): any {
     log.trace('TRACE:', args)
+    return args
 }
 
-function debug(...args: any[]): void {
+function debug(...args: any[]): any {
     log.debug('DEBUG:', args)
+    return args
 }
 
-function info(...args: any[]): void {
+function info(...args: any[]): any {
     log.info('INFO:', args)
+    return args
 }
 
-function warn(...args: any[]): void {
+function warn(...args: any[]): any {
     log.warn('WARN:', args)
+    return args
 }
 
-function error(...args: any[]): void {
+function error(...args: any[]): any {
     log.error('ERROR:', args)
+    return args
 }
 
 export {
