6af83983173f87cba1ff8df67d7ee1f63a3f4e02
Kalk <simon.d.mansson@gmail.com>
2020-04-14 13:04:41 +0200

    feat: 🎸 serve data-store from cloud functions
    With these changes data-store can now be served by cloud functions
    instead of app engine
    
    BREAKING CHANGE: 🧨 data-store url

--- firebase_serve.sh
+++ firebase_serve.sh
@@ -4,8 +4,17 @@ set -eou
 command -v npm >/dev/null 2>&1 || { echo >&2 "npm needs to be installed"; exit 1; }
 command -v firebase >/dev/null 2>&1 || { echo >&2 "firebase needs to be installed"; exit 1; }
 
+# Firebase functions needs to have all code reachable from the functions folder.
+# I.e we can't use relavtive paths back up from that directory
+# Clean up old server code
+rm -rf server/functions/src/data-store
+rm -rf server/functions/src/notifications
+
 npm --prefix client/ run build-dev
 npm --prefix server/notifications run build
 npm --prefix server/data-store run tsc-build
+# If all builds pass copy to functions src folder
+cp -r server/notifications/lib server/functions/src/notifications
+cp -r server/data-store/dist server/functions/src/data-store
 
 firebase serve
--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -eou
 command -v npm >/dev/null 2>&1 || { echo >&2 "npm needs to be installed"; exit 1; }
 
 npm --prefix client/ install
-npm --prefix server/notifications install
+npm --prefix server/functions install
 npm --prefix server/data-store install
 npm --prefix server/notifications install
